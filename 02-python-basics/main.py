999ea215f1f637e952e6a12c1622ce8f3cef7a35
Jovan Nel <neljovan88@gmail.com>
2022-08-04 16:55:36 +0100

    python basics

---
+++ 02-python-basics/main.py
@@ -0,0 +1,17 @@
+# Basic math arithmetic with operators using integers and float
+
+# print(type(2 + 4)) # Returns int type
+# print(type(2 - 4)) # Returns int type
+# print(type(2 * 4)) # Returns int type
+# print(type(2 / 4)) # Returns float type
+
+# print(type(int(20+1.1))) # Coerces type to float, can be offset by using int()
+
+# print(2 ** 8) # 2 to the power of 8
+# print(2//4) # Uses floor method to return a integer rounded down
+# print(5 % 4) # Modulo operator - return the reminder. Good for deciding if number is even or odd
+
+# Math Operations
+
+print(round(3.1)) # Rounds number down, can take second argument for nth digit rounding
+print(abs(-50)) # Returns absolute value
