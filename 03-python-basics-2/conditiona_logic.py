47c8fbdaa4714529100245790c252b03a83223bd
connorjnel <connor.j.nel00@gmail.com>
2022-08-12 14:18:46 +0100

    python basics 2

---
+++ 03-python-basics-2/conditiona_logic.py
@@ -0,0 +1,11 @@
+is_old = False
+is_licensed = True
+
+if is_old:
+    print("You are old enough to drive")
+elif is_licensed:
+    print("You can drive now")
+else:
+    print("You cannot drive")
+
+print("Test")
---
+++ 03-python-basics-2/python-basics-2.md
@@ -0,0 +1,5 @@
+# Python Basics 2
+
+## Conditional Logic
+
+Similar to JS, basic programming concept tbh, if condition: action, elif condition: action, else: action
