098f95ffc199bfae656baee4a99e2dc435cb02b6
connorjnel <connor.j.nel00@gmail.com>
2022-09-17 23:54:53 +0100

    modules

--- 09-python-modules/random_game.py
+++ 09-python-modules/random_game.py
@@ -1,8 +1,31 @@
 import random
+import sys
+
+# My Solution
 
 
-def random_game(num):
-    user_input = input("Guess a number between 0 and 10?")
-    number = random.randint(0, 10)
+# def random_game():
+#     number = random.randint(0, 10)
 
-    user_input = num
+#     # code_guess = sys.argv[0]
+
+#     # if code_guess:
+#     #     guess = int(code_guess)
+#     # else:
+#     user_input = input("Guess a number between 0 and 10?\n")
+#     guess = int(user_input)
+
+#     if guess < number:
+#         print(f"Guess too low, correct answer was {number}")
+#     elif guess > number:
+#         print(f"Guess too high, correct answer was {number}")
+#     elif guess == number:
+#         print(f"You are correct, correct answer was {number}")
+
+
+# random_game()
+
+
+# Provided Solution
+
+answer = random.randint(1, 10)
