cc46e855a281673b6599873a307713e3d3a45da1
connorjnel <connor.j.nel00@gmail.com>
2022-09-23 14:05:30 +0100

    file io

--- 11-python-filo-io/translator-exercise.py
+++ 11-python-filo-io/translator-exercise.py
@@ -3,13 +3,16 @@
 # install translate
 
 from translate import Translator
+translator = Translator(to_lang="ja")
 
 try:
-    with open("./test.txt", mode="r+") as source_file:
+    with open("./test.txt", mode="r") as source_file:
         source_text = source_file.read()
-        translator = Translator(to_lang="ja")
         translation = translator.translate(source_text)
         print(f"Original Text: {source_text}")
         print(f"Translation: {translation}")
+        # Write disabled due to encode error
+        # with open("text-ja.txt", mode="w") as my_file_ja:
+        #     my_file_ja.write(translation)
 except IOError:
     print("You messed up")
