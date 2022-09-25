de8782db7d01727052cbc330a6e6aea6594feb3e
connorjnel <connor.j.nel00@gmail.com>
2022-09-25 22:29:45 +0100

    testing

--- 13-python-testing/exercise/main.py
+++ 13-python-testing/exercise/main.py
@@ -1,43 +1,23 @@
-from random import randint
-import sys
-# generate a number 1~10
-answer = randint(1, 10)
-guess = int(input('guess a number 1~10:  '))
-user_guess = guess
-
-# input from user?
-# check that input is a number 1~10
-
-# Old Version
-# while True:
-#     try:
-#         guess = int(input('guess a number 1~10:  '))
-#         if 0 < guess < 11:
-#             if guess == answer:
-#                 print('you are a genius!')
-#                 break
-#         else:
-#             print('hey bozo, I said 1~10')
-#     except ValueError:
-#         print('please enter a number')
-#         continue
-
-# New version
+import random
 
 
-def guessing_game(user_guess):
+def run_guess(guess, answer):
+    if 0 < guess < 11:
+        if guess == answer:
+            print('you are a genius!')
+            return True
+    else:
+        print('hey bozo, I said 1~10')
+        return False
+
+
+if __name__ == '__main__':
+    answer = random.randint(1, 10)
     while True:
         try:
-            # guess = int(input('guess a number 1~10:  '))
-            if 0 < user_guess < 11:
-                if user_guess == answer:
-                    print('you are a genius!')
-                    break
-            else:
-                print('hey bozo, I said 1~10')
+            guess = int(input('guess a number 1~10:  '))
+            if (run_guess(guess, answer)):
+                break
         except ValueError:
             print('please enter a number')
             continue
-
-
-guessing_game(user_guess)
--- 13-python-testing/exercise/test.py
+++ 13-python-testing/exercise/test.py
@@ -1,18 +1,24 @@
-# Rewrite function to use FP
-# only test the guessing part
-
 import unittest
 import main
-from main import answer
-from main import guess
 
 
-class TestMain(unittest.TestCase):
-    def setUp(self):
-        print("About to test function")
+class TestGame(unittest.TestCase):
+    def test_input(self):
+        result = main.run_guess(5, 5)
+        self.assertTrue(result)
 
-    def test_do_stuff(self):
-        '''Basic test'''
-        test_param = 10
-        result = main.guessing_game(test_param)
-        self.assertEqual(result, 15)
+    def test_input_wrong_guess(self):
+        result = main.run_guess(0, 5)
+        self.assertFalse(result)
+
+    def test_input_wrong_number(self):
+        result = main.run_guess(11, 5)
+        self.assertFalse(result)
+
+    def test_input_string(self):
+        result = main.run_guess(5, "11")
+        self.assertFalse(result)
+
+
+if __name__ == '__main__':
+    unittest.main()
