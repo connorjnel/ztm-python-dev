3e57e60024aa8a159120cdfbc76082bf5c45aa00
connorjnel <connor.j.nel00@gmail.com>
2022-10-05 23:43:20 +0100

    scraping

--- 15-python-scraping/scrape.py
+++ 15-python-scraping/scrape.py
@@ -14,19 +14,27 @@ links = soup.select('.titleline')
 # Grab all votes items with score class
 subtext = soup.select('.subtext')
 
+# Sort stories by votes
+
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
 
 # Function for displaying the scraped data
+
 def create_custom_hn(links, subtext):
     hn = []
     for idx, item in enumerate(links):
         title = item.getText()
         href = item.get('href', None)
+        # TODO: Fix link not working, issue with getting correct selector
         vote = subtext[idx].select('.score')
         if len(vote):
             points = int(vote[0].getText().replace(' points', ''))
             if points > 99:
                 hn.append({'title': title, 'link': href, 'votes': points})
-    return hn
+    return sort_stories_by_votes(hn)
 
 
 pprint.pprint(create_custom_hn(links, subtext))
