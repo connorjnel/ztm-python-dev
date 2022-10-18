d27a742b8d45b8709906c46e3b1b4bc6a06d5aed
connorjnel <connor.j.nel00@gmail.com>
2022-10-18 13:22:24 +0100

    web-dev

--- 16-python-webdev/simple-website/venv/server.py
+++ 16-python-webdev/simple-website/venv/server.py
@@ -4,42 +4,12 @@ app = Flask(__name__)
 
 
 # Default home
-@app.route("/index.html")
+@app.route("/")
 def my_home():
     return render_template("./index.html")
 
 
-# Second home
-@app.route("/")
-def my_home2():
-    return render_template("./index.html")
-
-
-# About
-@app.route("/about.html")
-def about():
-    return render_template("./about.html")
-
-
-# Works
-@app.route("/works.html")
-def works():
-    return render_template("./works.html")
-
-
-# Works
-@app.route("/work.html")
-def work():
-    return render_template("./work.html")
-
-
-# Contact
-@app.route("/contact.html")
-def contact():
-    return render_template("./contact.html")
-
-
-# Components
-@app.route("/components.html")
-def components():
-    return render_template("./components.html")
+# Links
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
