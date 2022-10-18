b52c5f06625b7e03b79c9ed8a6ace2aa95d5c240
connorjnel <connor.j.nel00@gmail.com>
2022-10-18 13:04:45 +0100

    web-dev

--- 16-python-webdev/simple-website/venv/server.py
+++ 16-python-webdev/simple-website/venv/server.py
@@ -4,22 +4,42 @@ app = Flask(__name__)
 
 
 # Default home
-@app.route("/")
-def home():
+@app.route("/index.html")
+def my_home():
     return render_template("./index.html")
 
 
-# Home with variable rules
-# @app.route("/<username>/<int:post_id>")
-# def hello_world(username=None, post_id=None):
-#     return render_template("./index.html", name=username, post_id=post_id)
+# Second home
+@app.route("/")
+def my_home2():
+    return render_template("./index.html")
 
 
-@app.route("/blog")
-def blog():
-    return "<p>This is a blog</p>"
+# About
+@app.route("/about.html")
+def about():
+    return render_template("./about.html")
 
 
-@app.route("/blog/2022/cats")
-def cat_blog():
-    return "<p>This is a cat blog</p>"
+# Works
+@app.route("/works.html")
+def works():
+    return render_template("./works.html")
+
+
+# Works
+@app.route("/work.html")
+def work():
+    return render_template("./work.html")
+
+
+# Contact
+@app.route("/contact.html")
+def contact():
+    return render_template("./contact.html")
+
+
+# Components
+@app.route("/components.html")
+def components():
+    return render_template("./components.html")
