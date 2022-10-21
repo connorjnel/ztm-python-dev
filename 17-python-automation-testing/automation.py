6a1cc6be2f13e6b1ad80a5d3402ba0c9ea0fac77
connorjnel <connor.j.nel00@gmail.com>
2022-10-21 14:30:47 +0100

    selenium

--- 17-python-automation-testing/automation.py
+++ 17-python-automation-testing/automation.py
@@ -1,5 +1,18 @@
+import time
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 
 chrome_browser = webdriver.Chrome("./chromedriver")
+chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
 
-chrome_browser.maximize_window()
+sum1 = chrome_browser.find_element(By.ID, 'sum1')
+sum1.clear()
+sum1.send_keys('1')
+
+sum2 = chrome_browser.find_element(By.ID, 'sum2')
+sum2.clear()
+sum2.send_keys('2')
+
+time.sleep(2)
+get_total_button = chrome_browser.find_element(By.LINK_TEXT, 'Get Total')
+get_total_button.click()
