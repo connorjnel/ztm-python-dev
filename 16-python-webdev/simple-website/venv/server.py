cdf378b127a459c5b50589ea4c997cea4ee23712
connorjnel <connor.j.nel00@gmail.com>
2022-10-21 12:39:41 +0100

    web-dev

--- 16-python-webdev/simple-website/venv/server.py
+++ 16-python-webdev/simple-website/venv/server.py
@@ -15,12 +15,20 @@ def html_page(page_name):
     return render_template(page_name)
 
 
+def write_to_file(data):
+    with open("database.txt", mode="a") as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f"\n{email}, {subject},{message}")
+
+
 # Routing for form
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == "POST":
         data = request.form.to_dict()
-        print(data)
+        write_to_file(data)
         return redirect("/thankyou.html")
     else:
         return "Something went wrong, try again"
